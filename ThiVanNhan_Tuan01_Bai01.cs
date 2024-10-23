736da18e21a5c78eb96fc0f9ded1fc66a13931e9
Thi-Nhan <0306221452@caothang.edu.vn>
2024-10-23 22:44:14 +0700

    Add files via upload

---
+++ ThiVanNhan_Tuan01_Bai01.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            int largest = 0;
+
+            for (; counter < 10; counter++)
+            {
+                Console.Write($"Nhap vao so thu {counter + 1}: ");
+                int number = int.Parse(Console.ReadLine());
+                if (counter == 0 || number > largest)
+                {
+                    largest = number;
+                }
+            }
+            Console.WriteLine($"So lon nhat la: {largest}");
+
+            Console.ReadKey();
+        }
+    }
+}
